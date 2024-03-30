3e672c906b21e4c18b4513d5cc0c82521f8b5790
lather10 <latherdeepak766@gmail.com>
2024-03-31 03:01:30 +0530

    review the fact code

--- factorial/fact.cpp
+++ factorial/fact.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-int fact(int n)
+int fun(int n)
 {
 if(n==0)
     {
@@ -7,15 +7,33 @@ if(n==0)
     }
     else
     {
-        int j=n*fact(n-1);
-        std::cout<<j;
+            n=n*fun(n-1);
     }
 }
 int main()
 {
-    int n,j;
-    std::cout<<"Enter a number to find factorial: ";
+    int n;
+    char A,N;
+    std::cout<<"Enter a number to find Factorial: ";
     std::cin>>n;
-    fact(n);
+    std::cout<<"\n";
+    if(n==0)
+    {
+        return 1;
+    }
+    else if(n>0)
+    {
+        n=fun(n);
+        std::cout<<"Factorial of entered number is: ";
+        std::cout<<n<<"\n";
+    }
+    else
+    {
+        std::cout<<"\nEnter positive integer only\n";
+    }
+    std::cout<<"\nEnter A to find again for new number and N to exit: ";
+
+    //std::cin>>n;
+    //std::cout<<"\n";
     return 0;
 }
