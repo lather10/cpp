7950cc6ea90a7070f7edf6dbc7f8a05b680b67a2
Deepak Lather <164880032+lather10@users.noreply.github.com>
2024-04-04 01:12:34 +0530

    initial commit for factorial (#4)
    * initial commit for factorial
    
    * review the fact code
    
    * review the fact code
    
    * review this recent one by ignoring prev commits
    
    * review addressed
    
    * Remaining review addressed
    
    * Indentation fixed

---
+++ factorial/fact.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+uint64_t fact(int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    else
+    {
+            return n*fact(n-1);
+    }
+
+}
+int main()
+{
+    uint64_t result;
+    int input;
+    std::cout<<"Enter a positive Integer to find Factorial: ";
+    while(!(std::cin>>input))
+        {
+        std::cout<<"\nEnter positive Integer: ";
+        std::cin.clear();
+        std::cin.ignore(123,'\n');
+        }
+        if(input<0)
+        {
+            std::cout<<"\nEnter positive number only\n";
+        }
+        else
+        {
+            result=fact(input);
+            std::cout<<"\nNumber entered to find the factorial is: "<<input<<"\n";
+            std::cout<<"\nFactorial of number is: "<<result<<"\n";
+        }
+        return 0;
+}
